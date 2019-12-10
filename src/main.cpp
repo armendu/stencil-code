630b8b47ba474382a2319ecd4e12573cb4cd16a6
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-12-10 18:30:19 +0100

    Removed cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <openmp.h>
+#include "paralel_stencil_code.h"
 
 int main()
 {
@@ -11,89 +13,45 @@ int main()
 
   float **matrix = new float *[n_rows];
 
-  for (size_t i = 0; i < n_rows; i++)
-  {
-    float *row = new float[n_cols];
-    matrix[i] = row;
-  }
-
-  for (size_t i = 0; i < n_rows; i++)
-  {
-    for (size_t j = 0; j < n_cols; j++)
-    {
-      if (i == 0 && j == 0)
-      {
-        matrix[i][j] = 0;
-      }
-      else if (i == 0 && j > 0)
-      {
-        matrix[i][j] = 250;
-      }
-      else if (j == 0 && i > 0)
-      {
-        matrix[i][j] = 150;
-      }
-      else
-      {
-        matrix[i][j] = (abs(sin(matrix[i][j - 1])) +
-                        abs(sin(matrix[i - 1][j - 1])) +
-                        abs(sin(matrix[i - 1][j]))) * 100;
-      }
-    }
-  }
-
-#pragma omp parallel sections
+//  for (size_t i = 0; i < n_rows; i++)
 //  {
-//    for (i = 0; i < n_rows; i++)
+//    float *row = new float[n_cols];
+//    matrix[i] = row;
+//  }
+//
+//  for (size_t i = 0; i < n_rows; i++)
+//  {
+//    for (size_t j = 0; j < n_cols; j++)
 //    {
-//      #pragma omp section
+//      if (i == 0 && j == 0)
 //      {
-//        float *row = new float[n_cols];
-//
-//        for (int j = 0; j < n_cols; j++)
-//        {
-//          if (i == 0 && j == 0)
-//          {
-//            row[j] = 0;
-//          }
-//          else if (i == 0 && j > 0)
-//          {
-//            row[j] = 250;
-//          }
-//          else
-//          {
-//            float *prev_row = matrix[i - 1];
-//            row[j] = (abs(sin(prev_row[j - 1])) + abs(sin(row[j - 1])) + abs(sin(prev_row[j]))) * 100;
-//          }
-//        }
-//        matrix[i] = row;
+//        matrix[i][j] = 0;
 //      }
-//
-//      #pragma omp section
+//      else if (i == 0 && j > 0)
 //      {
-//        float *row = new float[n_cols];
-//
-//        for (int j = i; j < n_cols; j++)
-//        {
-//          if (i == 0 && j == 0)
-//          {
-//            row[j] = 0;
-//          }
-//          else if (i == 0 && j > 0)
-//          {
-//            row[j] = 250;
-//          }
-//          else
-//          {
-//            float *prev_row = matrix[j - 1];
-//            row[j] = (abs(sin(prev_row[j - 1])) + abs(sin(row[j - 1])) + abs(sin(prev_row[j]))) * 100;
-//          }
-//
-//          matrix[j] = row;
-//        }
+//        matrix[i][j] = 250;
+//      }
+//      else if (j == 0 && i > 0)
+//      {
+//        matrix[i][j] = 150;
+//      }
+//      else
+//      {
+//        matrix[i][j] = (abs(sin(matrix[i][j - 1])) +
+//                        abs(sin(matrix[i - 1][j - 1])) +
+//                        abs(sin(matrix[i - 1][j]))) * 100;
 //      }
 //    }
 //  }
+
+#pragma omp parallel num_threads(3)
+  {
+    printf("https://helloacm.com\n");
+  }
+
+  paralel_stencil_code* paralelStencilCode = new paralel_stencil_code();
+
+  paralelStencilCode->run();
 
   return 0;
 }
